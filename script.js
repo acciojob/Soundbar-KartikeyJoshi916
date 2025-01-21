dd5840d90b977aece7ef7301edfcb6cb7e2c69ba
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-21 16:46:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const sounds = {};
+
+buttons.forEach(button => {
+    const soundName = button.getAttribute('data-sound');
+    sounds[soundName] = new Audio(`sounds/${soundName}.mp3`);
+
+    button.addEventListener('click', () => {
+        stopAllSounds();
+        sounds[soundName].play();
+    });
+});
+
+stopButton.addEventListener('click', stopAllSounds);
+
+function stopAllSounds() {
+    Object.values(sounds).forEach(sound => {
+        sound.pause();
+        sound.currentTime = 0;
+    });
+}
